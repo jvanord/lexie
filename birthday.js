6bf446578564c70d5fd9ba4ba237a6a06893213b
Jay <jvanord@indasysllc.com>
2021-07-01 22:13:15 -0400

    final?

--- birthday.js
+++ birthday.js
@@ -1,26 +1,62 @@
-const TARGET_DATE = new Date(2021, 7, 5);
-let COUNT_DOWN = new Date(TARGET_DATE - new Date());
-(function ($) {
-    $(function () {
-        if (COUNT_DOWN > 0 && window.location.hash !== '#test') {
-            updateTimer();
-            $('#timer').show();
-            $('#card').hide();
-            setInterval(updateTimer, 500);
-        }
-        else {
-            $('#timer').hide();
-            $('#card').show();
-        }
-    });
-})(window.jQuery || console.error('jQuery broken'));
+const TARGET_DATE = new Date('July 5, 2021');
+(function ($) { $(runApp); })(window.jQuery || console.error('jQuery broken'));
 
-function updateTimer() {
-    let newDiff = TARGET_DATE - new Date();
-    COUNT_DOWN = newDiff > 0 ? new Date(newDiff) : new Date(0);
-    $('#timer-days').text(`${COUNT_DOWN.getDate()} DAYS`);
-    let h = COUNT_DOWN.getHours().toString().padStart(2, '0');
-    let m = COUNT_DOWN.getMinutes().toString().padStart(2, '0');
-    let s = COUNT_DOWN.getSeconds().toString().padStart(2, '0');
+function runApp() {
+    let countDownUntil = window.location.hash === '#test' ? secondsFromNow(10) : TARGET_DATE;
+    let countDownTimeSpan = window.countdown(countDownUntil);
+    if (countDownTimeSpan.value < 0) {
+        updateTimeDisplay(countDownTimeSpan);
+        let timer = setInterval(function () {
+            countDownTimeSpan = window.countdown(countDownUntil);
+            if (countDownTimeSpan.value < 0)
+                updateTimeDisplay(countDownTimeSpan);
+            else {
+                timer = clearInterval(timer);
+                showCard();
+            }
+        }, 500);
+    }
+    else {
+        showCard();
+    }
+}
+
+function secondsFromNow(seconds) {
+    var date = new Date();
+    date.setSeconds(date.getSeconds() + 10);
+    return date;
+}
+
+function updateTimeDisplay(countDownTimeSpan) {
+    $('#card').hide();
+    $('#timer-days').text(`${countDownTimeSpan.days.toString()} DAYS`);
+    let h = countDownTimeSpan.hours.toString().padStart(2, '0');
+    let m = countDownTimeSpan.minutes.toString().padStart(2, '0');
+    let s = countDownTimeSpan.seconds.toString().padStart(2, '0');
     $('#timer-countdown').text(`${h}:${m}:${s}`)
+    $('#timer').show();
+}
+
+function showCard() {
+    $('#timer').hide();
+    $('#card').show();
+    randomConfetti();
+    setInterval(randomSparkles, 2010);
+    setInterval(randomConfetti, 1530);
+}
+
+function randomConfetti() {
+    randomizeEmitter();
+    party.confetti(document.getElementById('emitter'));
+}
+
+function randomSparkles() {
+    randomizeEmitter();
+    party.sparkles(document.getElementById('emitter'));
+}
+
+function randomizeEmitter() {
+    let x = Math.random() * $(document).width();
+    let y = Math.random() * $(document).height();
+    $('#emitter').css({ top: y, left: x });
 }
